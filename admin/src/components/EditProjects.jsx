c9462e66f9c3e66440eb420f6c6c6a525f8ef266
PrathameshNi <nivdekarprathamesh3@gmail.com>
2026-06-18 12:28:49 +0530

    Update EditProjects.jsx

--- admin/src/components/EditProjects.jsx
+++ admin/src/components/EditProjects.jsx
@@ -94,7 +94,7 @@ const EditProjects = () => {
       );
 
       await axios.post(
-        "http://localhost:5000/api/projects",
+        "https://avconstructions.onrender.com/api/projects",
 
         formData,
 
