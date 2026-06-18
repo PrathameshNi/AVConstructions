fdbe6bd59f313b2894349b139cf3f20993f770a3
PrathameshNi <nivdekarprathamesh3@gmail.com>
2026-06-18 11:19:01 +0530

    Update AdminLogin.jsx

--- admin/src/components/AdminLogin.jsx
+++ admin/src/components/AdminLogin.jsx
@@ -17,7 +17,7 @@ const AdminLogin = () => {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/login",
+        "https://avconstructions.onrender.com/api/login",
         form
       );
 
