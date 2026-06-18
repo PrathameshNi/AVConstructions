86b9a211ed456655d9efc1458f59cb7eae691119
PrathameshNi <nivdekarprathamesh3@gmail.com>
2026-06-18 12:31:49 +0530

    Update EditAbout.jsx

--- admin/src/components/EditAbout.jsx
+++ admin/src/components/EditAbout.jsx
@@ -105,7 +105,7 @@ const EditAbout = () => {
       }
 
       await axios.put(
-        "http://localhost:5000/api/about",
+        "https://avconstructions.onrender.com/api/about",
 
         formData,
 
