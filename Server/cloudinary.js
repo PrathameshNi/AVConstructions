d31ddfabd70374b971e68225699ae50d1cd71a9f
PrathameshNi <nivdekarprathamesh3@gmail.com>
2026-06-19 22:55:27 +0530

    update

--- Server/cloudinary.js
+++ Server/cloudinary.js
@@ -6,11 +6,4 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_SECRET,
 });
 
-
 module.exports = cloudinary;
-<<<<<<< HEAD
-=======
-
->>>>>>> e1f93cf3f43ef12e365ca378131b441f4ebaa2af
-module.exports = cloudinary;
-
