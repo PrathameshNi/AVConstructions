fa8bcdc2ae81b75cab065eb2aabb7c9ef8c92da1
PrathameshNi <nivdekarprathamesh3@gmail.com>
2026-06-20 19:49:59 +0530

    updates

--- client/src/pages/Map.jsx
+++ client/src/pages/Map.jsx
@@ -4,7 +4,6 @@ import "./Style2.css";
 const Map = () => {
   return (
     <section id="location" className="map-section">
-
       <div className="map-container">
 
         <h2>
@@ -12,22 +11,24 @@ const Map = () => {
         </h2>
 
         <p className="map-text">
-          Visit our office for project discussions,
-          consultations, and construction planning.
+          Visit our office at Lanja, Maharashtra for project
+          consultations and construction planning.
         </p>
 
         <iframe
-          title="AV Construction Location"
-          src="https://www.google.com/maps/embed?pb=<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d122197.54637060444!2d73.45949828350653!3d16.842554676559256!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc1d97afe28c4df%3A0x288883dfb523a4f7!2sLanja%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1781964007613!5m2!1sen!2sin" 
+          title="Lanja Maharashtra Location"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d122197.54637060444!2d73.45949828350653!3d16.842554676559256!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc1d97afe28c4df%3A0x288883dfb523a4f7!2sLanja%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1781965116211!5m2!1sen!2sin"
+          width="100%"
           height="450"
-          style={{ border: 0 }}
-          allowFullScreen=""
+          style={{
+            border: 0,
+            borderRadius: "15px",
+          }}
+          allowFullScreen
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
-        ></iframe>
-
+        />
       </div>
-
     </section>
   );
 };
